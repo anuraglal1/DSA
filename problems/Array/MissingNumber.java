8993730f3114715e352665db0d7edc0ddf21876a
anuraglal <anuraglal77@gmail.com>
2025-12-13 23:26:20 +0530

    Missing number.

---
+++ problems/Array/MissingNumber.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+        int i;
+        int n = nums.length;
+
+        int sum = Arrays.stream(nums).sum();
+        int expected = (n * (n+1))/2;
+
+        return expected - sum;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 3};
+        System.out.println("Missing number is: " + missingNumber(nums));
+    }
+}
